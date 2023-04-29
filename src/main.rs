57c385a03148a0f7820d07ce4be1bf99653c46f8
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-04-29 22:36:39 +0300

    types for to store posts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::time::SystemTime;
+//ds that represent the site we want to generate
+
+pub struct SiteMetadata{
+    title: String,
+    description: String,
+    date: SystemTime,
+}
+
+impl SiteMetadata {
+    fn new(
+        title: String,
+        description: String,
+        date: SystemTime,
+    ) -> Self {
+        SiteMetadata {
+            title,
+            description,
+            date,
+        }
+    }
+}
+pub struct Posts {
+    post: PathBuf,
+}
+
+impl Posts {
+    fn new(post: PathBuf) -> Self {
+        Posts {
+            post,
+     }
+    }
+}
+pub struct Pages {
+    content: String,
+}
+
+pub struct Tags {
+    tags: String,
+}
+
+
+
+fn main () {
+
 }
