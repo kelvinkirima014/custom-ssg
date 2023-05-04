8fb690b2855c34e3182d652e09eb9f7fc6bacf29
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-05-04 09:55:27 +0300

    serve files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::collections::BTreeMap;
 use std::io::{Error, Read};
 use std::fs::{self, File};
 use std::path::PathBuf;
 use std::time::SystemTime;
+use std::io::Write;
+use handlebars::Handlebars;
+use hyper::service::service_fn;
+use hyper::{ Body, Request, Response, Server, service};
+use serde_json::json;
 //ds that represent the site we want to generate
 
 #[allow(dead_code)]
@@ -11,28 +17,7 @@ pub struct SiteMetadata{
     date: SystemTime,
 }
 
-#[allow(dead_code)]
-impl SiteMetadata {
-    fn new(
-        title: String,
-        description: String,
-        date: SystemTime,
-    ) -> Self {
-        SiteMetadata {
-            title,
-            description,
-            date,
-        }
-    }
 
-    // fn fetch_metadata(&self, filepath: PathBuf) -> Result<Self, Error>{
-    //     let content = fs::read_to_string(&filepath);
-    //     let metadata: Self = serde_yaml::from_str(&content)?;
-
-
-    //     Ok(())
-    // }
-}
 #[allow(dead_code)]
 pub struct Posts {
     post_path: PathBuf,
@@ -43,7 +28,7 @@ impl Posts {
     fn new(post_path: PathBuf) -> Self {
         Posts {
             post_path,
-     }
+        }
     }
 
     fn fetch_posts(&self) -> Result<Vec<(PathBuf, String)>, Error>{
@@ -52,6 +37,7 @@ impl Posts {
 
         let files = fs::read_dir(&self.post_path)?;
 
+        let mut handlebars = Handlebars::new();
             //iterate over the contents of the directory
             for file in files {
                 let file = file?;
@@ -72,6 +58,20 @@ impl Posts {
     }
 }
 
+use pulldown_cmark::{html, Options, Parser};
+
+fn md_to_html(markdown: &str) -> String {
+    // Set up the parser with default options
+    let parser = Parser::new_ext(markdown, Options::empty());
+
+    // Convert the markdown to HTML
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
+    html_output
+}
+
+
 fn main () {
     let post_path = PathBuf::from("./markdown");
     let posts = Posts::new(post_path);
@@ -87,4 +87,28 @@ fn main () {
         }
     }
 
+    let post_contents = posts;
+
+    let input_src = "<html><body>{{{content}}}</body></html>";
+
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_string("a_template", input_src).unwrap();
+
+    let html = handlebars.render("a_template",&json!({"content": md_to_html(post_contents)})).unwrap();
+    
+    let mut file = fs::File::create("index.html").unwrap();
+    file.write_all(html.as_bytes()).unwrap();
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr)
+        .serve(|| service_fn(|_req| {
+            let content = fs::read("index.html").unwrap();
+            Response::new(Body::from(content))
+        }))
+        .map_err(|e| eprintln!("Server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+    hyper::rt::run(server);
+  
 }
