93f2fc091633f9fe74d42a9ca1d95d4ed793950e
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-06-30 12:35:28 +0300

    serve_html

--- src/server.rs
+++ src/server.rs
@@ -1,44 +1,41 @@
-use std::convert::Infallible;
-use std::error::Error;
-use std::fs;
-use std::path::PathBuf;
-use std::sync::Arc;
+use std::{sync::Arc, path::PathBuf, fs};
 
-use hyper::{ Body, Request, Response, StatusCode };
-use hyper::service::service_fn;
+use hyper::{Body, Response, Request, StatusCode};
 
-async fn serve_html_files(req: Request<Body>) -> Result<(), Box<dyn Error>> {
-    let req = Arc::new(req);
-    let service = service_fn(move |_: Request<Body>| {
-    let cloned_req = Arc::clone(&req);
-         async move {
-            let path = cloned_req.uri().path().to_owned();
-            let content_dir = Arc::new(PathBuf::from(".."));
-            if let Some(file_name) = path.strip_prefix("/") {
-               let file_path = content_dir.join(file_name);
-               if file_path.is_file() {
-                match fs::read(file_path) {
-                    Ok(content) => {
-                        let response = Response::new(Body::from(content));
-                        
-                    }
-                    Err(err) => {
-                        eprintln!("Failed to read file: {}", err);
-                        let response = Response::builder()
-                         .status(StatusCode::INTERNAL_SERVER_ERROR)
-                         .body(Body::empty())
-                         .unwrap();
-                    }   
-                }
-                
-               } 
+
+
+async fn serve_html(
+    req: Request<Body>,
+    content_dir: Arc<PathBuf>,
+) -> Result<Response<Body>, hyper::Error> {
+
+    let path = req.uri().path();
+    let file_name = path.strip_prefix("").unwrap();
+    let file_path = content_dir.join(file_name);
+    
+    if file_path.is_file(){
+        match fs::read(file_path){
+            Ok(contents) => {
+                let response = Response::builder()
+                    .status(StatusCode::OK)
+                    .body(Body::from(contents))
+                    .unwrap();
+                return Ok(response);
             }
-            
+            Err(err) => {
+                eprintln!("Failed to read file: {}", err);
+                let error_response = Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::empty())
+                    .unwrap();
+                return Ok(error_response);
+            }
         }
-
+    } else {
+        let response = Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap();
+        Ok(response)
     }
-       
-    );
-
-    Ok(())
 }
