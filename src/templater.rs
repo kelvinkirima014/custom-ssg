fbd8b1e1b55e9ea488f98c5eaa2d521ea7d6cfac
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-08-24 09:08:18 +0300

    msc

--- src/templater.rs
+++ src/templater.rs
@@ -4,10 +4,53 @@ use pulldown_cmark::{ Parser, Options, html };
 use serde_yaml;
 use serde_json::{self, json};
 use std::io;
+use regex::Regex;
 
+use syntect::html::highlighted_html_for_string;
+use syntect::parsing::SyntaxSet;
+use syntect::highlighting::ThemeSet;
 
 fn md_to_html(markdown: &str) -> String {
-    let parser = Parser::new_ext(markdown, Options::empty());
+
+    let syntax_set = SyntaxSet::load_defaults_newlines();
+
+    let theme_set = ThemeSet::load_defaults();
+
+    let syntax_option = syntax_set.find_syntax_by_token("rust");
+    let theme_option = theme_set.themes.get("base16-ocean.light");
+    assert!(syntax_option.is_some() && theme_option.is_some(), "Failed to load syntax or theme");
+
+    let highlight_code = |code: &str, lang: &str| -> String {
+        let syntax = match lang {
+            "rust" => syntax_set.find_syntax_by_token("rust"),
+            "javascript" => syntax_set.find_syntax_by_token("javascript"),
+            "typescript" => syntax_set.find_syntax_by_token("typescript"),
+            _ => None,
+        
+        };
+
+        if let Some(s) = syntax {
+            highlighted_html_for_string(code, &syntax_set, s, &theme_set.themes["base16-ocean.dark"]).unwrap()
+        } else {
+            code.to_string()
+        }
+
+    };
+
+    let tags = ["rust", "javascript", "typescript"];
+
+    let mut result_md = markdown.to_string();
+
+    for tag in &tags {
+        let re = Regex::new(&format!(r#"<code class="{}">(.*?)</code>"#, tag)).unwrap();
+
+        result_md = re.replace_all(&result_md, |caps: &regex::Captures| {
+            let code = &caps[1];
+            format!("<code>{}</code>", highlight_code(code, tag))
+        }).to_string();
+    }
+
+    let parser = Parser::new_ext(&result_md, Options::empty());
     
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
